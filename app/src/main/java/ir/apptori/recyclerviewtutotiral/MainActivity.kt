25d7100e85b3e6bb1ebe841d9df990e07d4a4da1
Hossein Amini <hosseinamiinii@gmail.com>
2020-11-01 18:47:39 +0330

    Init

---
+++ app/src/main/java/ir/apptori/recyclerviewtutotiral/MainActivity.kt
@@ -0,0 +1,11 @@
+package ir.apptori.recyclerviewtutotiral
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
