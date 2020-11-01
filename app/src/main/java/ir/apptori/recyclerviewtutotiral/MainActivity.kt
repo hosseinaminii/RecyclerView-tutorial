088e2db62ad628fe8cfe59db38198456f7ea5554
Hossein Amini <hosseinamiinii@gmail.com>
2020-11-01 18:57:46 +0330

    Connecting all pieces together

--- app/src/main/java/ir/apptori/recyclerviewtutotiral/MainActivity.kt
+++ app/src/main/java/ir/apptori/recyclerviewtutotiral/MainActivity.kt
@@ -2,10 +2,55 @@ package ir.apptori.recyclerviewtutotiral
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rclNames = findViewById<RecyclerView>(R.id.rclNames)
+
+        // If size of the all items are equal and won't change for a better performance it's better to set setHasFixedSize to true
+        rclNames.setHasFixedSize(true)
+
+        // Creating an instance of our NameAdapter class and setting it to our RecyclerView
+        val nameList = getListOfNames()
+        val namesAdapter = NameAdapter(nameList)
+        rclNames.adapter = namesAdapter
+        // Setting our RecyclerView's layout manager equal to LinearLayoutManager
+        rclNames.layoutManager = LinearLayoutManager(this)
+
+    }
+
+    // This function just creates a list of names for us
+    private fun getListOfNames(): MutableList<String> {
+        val nameList = mutableListOf<String>()
+        nameList.add("Ali")
+        nameList.add("Sophia")
+        nameList.add("Isabella")
+        nameList.add("Mason")
+        nameList.add("Jacob")
+        nameList.add("William")
+        nameList.add("Olivia")
+        nameList.add("Jayden")
+        nameList.add("Chloe")
+        nameList.add("Ella")
+        nameList.add("Anthony")
+        nameList.add("Joshua")
+        nameList.add("James")
+        nameList.add("Grace")
+        nameList.add("Samantha")
+        nameList.add("Nicholas")
+        nameList.add("Brianna")
+        nameList.add("Justin")
+        nameList.add("Lauren")
+        nameList.add("Kimberly")
+
+        return nameList
     }
 }
